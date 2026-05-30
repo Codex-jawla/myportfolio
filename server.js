c6442e661468d5aa402b9c5773e369368fe2896e
HIMANSHU JAWLA <101585686+Codex-jawla@users.noreply.github.com>
2026-05-30 14:33:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 const corsOptions = {
   origin: 'https://myfolioz.vercel.app',
-  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS','Access-Control-Allow-Origin'],
   allowedHeaders: ['Content-Type', 'Authorization',"x-admin-key"],
   credentials: true,
 };
